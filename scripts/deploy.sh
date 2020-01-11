4e6a4692fb307df6a44691a7afbb4892164b1dd1
stmkza <stmkza@stmkza.net>
2020-01-11 19:04:50 +0900

    改行

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ fi
 ln -s "${FILES_DIR}/.zsh" "${HOME}/.zsh"
 ln -s "${FILES_DIR}/.zsh.d" "${HOME}/.zsh.d"
 ln -s "${FILES_DIR}/.zshrc" "${HOME}/.zshrc"
+
 ln -s "${FILES_DIR}/.gitconfig" "${HOME}/.gitconfig"
 ln -s "${FILES_DIR}/.gitignore_global" "${HOME}/.gitignore_global"
 
